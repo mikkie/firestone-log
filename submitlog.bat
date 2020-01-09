091011f3fe1824ef4f1f2c97a4dfd3eb6f22ee49
mikkie <lmj3648xm@hotmail.com>
2020-01-09 16:27:50 +0800

    remove bat

--- submitlog.bat
+++
@@ -1,5 +0,0 @@
-xcopy C:\Users\benjaminl\Desktop\firestone-v1.0\server\logs server /y
-xcopy C:\Users\benjaminl\Desktop\firestone-v1.0\server\shell\logs shell /y
-git add *
-git commit -m 'logs'
-git push origin master
