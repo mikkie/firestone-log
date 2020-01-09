a80780cda54cd5d63efeeb60378dac0217616bc3
Benjamin Li <benjamin.li@ehealth.com>
2020-01-09 15:49:49 +0800

    'logs'

---
+++ submitlog.bat
@@ -0,0 +1,5 @@
+xcopy C:\Users\benjaminl\Desktop\firestone-v1.0\server\logs server
+xcopy C:\Users\benjaminl\Desktop\firestone-v1.0\server\shell\logs shell
+git add *
+git commit -m 'logs'
+git push origin master
