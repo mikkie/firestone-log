b227ce0bc20f69fd43893d17aae8c8feda5dd578
mikkie <lmj3648xm@hotmail.com>
2020-01-12 17:46:40 +0800

    'db'

---
+++ exportData.bat
@@ -0,0 +1,4 @@
+mongoexport -d firestone -c hot_concept -o db/hot_concept.json
+git add *
+git commit -m 'db'
+git push origin master
---
+++ run.bat
@@ -0,0 +1,5 @@
+xcopy C:\Users\xiaxue\Desktop\firestone\server\logs server /y
+xcopy C:\Users\xiaxue\Desktop\firestone\server\shell\logs shell /y
+git add *
+git commit -m 'logs'
+git push origin master
